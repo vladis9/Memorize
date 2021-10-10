35598fe2ec32e9608ea067dba4b467525e9c66c2
Vlad Zinevich <vladisznv@gmail.com>
2021-10-10 13:02:29 +0300

    end lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,30 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🐢","🐱", "🐇", "🐶", "🐹", "🦊", "🐼", "🐭", "🐨", "🐯", "🐮", "🦁", "🐷", "🐸", "🐵", "🐤", "🐔", "🦉",]
+    @State var emojiCount = 15
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        
         .padding(.horizontal)
-        .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
     
 }
 
 struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill()
-                .foregroundColor(.white)
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("🐶")
-                .font(.largeTitle)
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
         }
     }
 }
